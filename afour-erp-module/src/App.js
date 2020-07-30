a351c1a4380d8128104c1e3f3a2aa315521e4138
krits29 <31025768+krits29@users.noreply.github.com>
2020-07-30 13:11:38 -0700

    Dropdown functionality

--- afour-erp-module/src/App.js
+++ afour-erp-module/src/App.js
@@ -40,8 +40,30 @@ class App extends Component {
     return (
       <div >
         <h1>Default COE view</h1>
-        <button className="year">Year</button>
-        <button className="quarter">Quarter</button>
+        
+        <div className="dropdown">
+          <button >Select Year</button>
+          <div className="dropdown-content">
+            <a href="#">2021-22</a>
+            <a href="#">2020-21</a>
+            <a href="#">2019-20</a>
+          </div>
+        </div>
+
+        <div className="dropdown">
+          <button >Select Cycle</button>
+          <div className="dropdown-content">
+            <a href="#">Jan-Mar</a>
+            <a href="#">Apr-Jun</a>
+            <a href="#">Jul-Sep</a>
+            <a href="#">Oct-Dec</a>
+            <a href="#">Apr</a>
+            <a href="#">Oct</a>
+          </div>
+        </div>
+
+        <button className="list">List Employees</button>
+
         <div className = "row">
           <div className="column">Employee ID</div>
           <div className="column">Name</div>
